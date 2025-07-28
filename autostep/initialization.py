10c9e83625abe4272675b608325c6cccc27b667b
Miguel Biron-Lattes <miguelbl9@gmail.com>
2025-07-28 14:25:19 -0700

    better optim init + pin jax

--- autostep/initialization.py
+++ autostep/initialization.py
@@ -1,4 +1,5 @@
 from functools import partial
+from copy import deepcopy
 
 import jax
 from jax import lax
@@ -48,42 +49,46 @@ def optimize_init_params(
         inv_temp=inv_temp
     )
 
+    # TODO: maybe use the final optimizer state to tune samplers (e.g. preconditioner)
+    return optimize_fun(
+        target_fun, 
+        init_params, 
+        deepcopy(initialization_settings) # safer since we `pop` stuff from it
+    )[0]
+
+# simple scan-based optimization loop
+def optimize_fun(
+        target_fun, 
+        init_params, 
+        initialization_settings
+    ):
+
     # select solver
+    solver = initialization_settings['strategy']
+    solver_params = initialization_settings['params']
+    n_iter = solver_params.pop('n_iter')
     if initialization_settings['strategy'] == "L-BFGS":
-        solver, scan_fn = make_lbfgs_solver(target_fun)
+        solver, scan_fn = make_lbfgs_solver(target_fun, solver_params)
     elif initialization_settings['strategy'] == "ADAM":
-        solver, scan_fn = make_adam_solver(target_fun)
+        solver, scan_fn = make_adam_solver(target_fun, solver_params)
     else:
         raise ValueError(
             f"Unknown strategy '{initialization_settings['strategy']}'"
         )
-
-    # can reuse stuff because target is deterministic
-    # see https://optax.readthedocs.io/en/stable/api/optimizers.html#lbfgs
-    cached_value_and_grad = optax.value_and_grad_from_state(target_fun)
-
-    # use L-BFGS to search for better initial point
-    solver = optax.lbfgs()
-    def scan_fn(carry, _):
-        params, opt_state = carry
-        value, grad = cached_value_and_grad(params, state=opt_state)
-        updates, opt_state = solver.update(
-            grad, opt_state, params, value=value, grad=grad, value_fn=target_fun
-        )
-        params = optax.apply_updates(params, updates)
-        return (params, opt_state), None
+    
+    # optimization loop
     print(f'Initial energy: {target_fun(init_params):.1e}')
     opt_params, opt_state = lax.scan(
         scan_fn, 
         (init_params, solver.init(init_params)),
-        length = initialization_settings['params']['n_iter']
+        length = n_iter
     )[0]
     print(f'Final energy: {target_fun(opt_params):.1e}')
     
-    return opt_params
+    return opt_params, opt_state
 
 # L-BFGS
-def make_lbfgs_solver(target_fun):
+def make_lbfgs_solver(target_fun, solver_params):
     print(f'Using L-BFGS to improve initial state.')
 
     # can reuse stuff because target is deterministic
@@ -91,7 +96,7 @@ def make_lbfgs_solver(target_fun):
     cached_value_and_grad = optax.value_and_grad_from_state(target_fun)
 
     # build solver and loop function
-    solver = optax.lbfgs()
+    solver = optax.lbfgs(**solver_params)
     def scan_fn(carry, _):
         params, opt_state = carry
         value, grad = cached_value_and_grad(params, state=opt_state)
@@ -104,9 +109,10 @@ def make_lbfgs_solver(target_fun):
     return solver, scan_fn
 
 # ADAM
-def make_adam_solver(target_fun):
+def make_adam_solver(target_fun, solver_params):
     print(f'Using ADAM to improve initial state.')
-    solver = optax.adam(learning_rate=0.003) # LR is from the example in docs
+    learning_rate = solver_params.pop('learning_rate', 0.003) # default LR is from the example in docs
+    solver = optax.adam(learning_rate=learning_rate, **solver_params)
     def scan_fn(carry, _):
         params, opt_state = carry
         grad = jax.grad(target_fun)(params)
