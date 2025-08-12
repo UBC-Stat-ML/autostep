b94b90445bd748e94d4a540117aa5a766ae5e8f5
Miguel Biron-Lattes <miguelbl9@gmail.com>
2025-08-12 15:20:14 -0700

    MAP util

--- tests/test_initialization.py
+++ tests/test_initialization.py
@@ -17,6 +17,8 @@ from autostep import autorwmh
 from autostep import preconditioning
 from autostep import selectors
 from autostep import statistics
+from autostep import initialization
+from autostep import tempering
 from autostep import utils
 
 
@@ -54,6 +56,31 @@ class TestInitialization(unittest.TestCase):
             print(max_rhats)
             self.assertTrue(max_rhats[0] >= max_rhats[1])
             self.assertTrue(max_rhats[0] >= max_rhats[2])
+
+    def test_MAP(self):
+        rng_key = random.key(90)
+        model, model_args, model_kwargs = testutils.make_eight_schools()
+        MAP_estimate_unconstrained = initialization.MAP(
+            model,
+            rng_key,
+            model_args,
+            model_kwargs,
+            options = {'strategy': "L-BFGS", 'params': {'n_iter': 64}}
+        )[0]
+
+        # transform to constrained space
+        exec_trace = tempering.trace_from_unconst_samples(
+            model, 
+            model_args, 
+            model_kwargs,
+            MAP_estimate_unconstrained, 
+        )
+        MAP_estimate = {
+            name: site["value"] for name, site in exec_trace.items() 
+            if site["type"] == "sample" and not site["is_observed"]
+        }
+        self.assertTrue(jnp.allclose(MAP_estimate['mu'], MAP_estimate['theta']))
+        self.assertLess(MAP_estimate['tau'], 1e-6)
 
 if __name__ == '__main__':
     unittest.main()
